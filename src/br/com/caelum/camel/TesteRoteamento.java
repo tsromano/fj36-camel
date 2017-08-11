af7e290486eea7612cc2fb16e37e5519433320cf
tsromano <thromano@gmail.com>
2017-08-11 02:09:02 -0300

    Initial commit

---
+++ src/br/com/caelum/camel/TesteRoteamento.java
@@ -0,0 +1,36 @@
+package br.com.caelum.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class TesteRoteamento {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		CamelContext context = new DefaultCamelContext();
+
+		context.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				errorHandler(
+				deadLetterChannel("file:falha").
+				useOriginalMessage().
+				maximumRedeliveries(2).
+				redeliveryDelay(2000).
+				retryAttemptedLogLevel(LoggingLevel.ERROR)
+										);
+				from("file:entrada?delay=5s")
+				.log(LoggingLevel.INFO, "Processando mensagem ${id}")
+				.to("validator:file:xsd/pedido.xsd")
+				.to("file:saida");
+			}
+		});
+		context.start();
+		Thread.sleep(30 * 1000);
+		context.stop();
+
+	}
+
+}
